f099c9d997816c3253260982dafb1c356be11628
Luis Falero <lfalero@redhat.com>
2022-06-08 19:17:23 -0500

    first commit

---
+++ src/main/java/com/lfalero/hellojava/BsHelloJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lfalero.hellojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsHelloJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsHelloJavaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lfalero/hellojava/HelloController.java
@@ -0,0 +1,18 @@
+package com.lfalero.hellojava;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("bs")
+public class HelloController {
+
+    @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UxResponseDto hello() {
+        String host = System.getenv().getOrDefault("HOSTNAME", "unknown");
+        String description = "Hello world from host API BS";
+        return new UxResponseDto(host, description);
+    }
+}
