7afdeeb4f08bf5db19548544ced0ec25a9829ae0
Luis Falero <lfalero@redhat.com>
2022-08-20 21:20:58 -0500

    Add version

--- src/main/java/com/lfalero/hellojava/HelloController.java
+++ src/main/java/com/lfalero/hellojava/HelloController.java
@@ -9,10 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("bs")
 public class HelloController {
 
-    @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-    public BsResponseDto hello() {
+    @GetMapping(value = "/v1/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    public BsResponseDto helloV1() {
         String host = System.getenv().getOrDefault("HOSTNAME", "unknown");
-        String description = "Hello world from host API BS";
+        String description = "Hello world from host API v1 BS";
+        return new BsResponseDto(host, description);
+    }
+
+    @GetMapping(value = "/v2/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    public BsResponseDto helloV2() {
+        String host = System.getenv().getOrDefault("HOSTNAME", "unknown");
+        String description = "Hello world from host API v2 BS";
         return new BsResponseDto(host, description);
     }
 }
